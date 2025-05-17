2a15a0d821aa9a1eefd8c6155b75c16c9d794451
lukezell <47638095+lukezell@users.noreply.github.com>
2025-05-16 23:40:38 -0500

    Make it Work

--- script.js
+++ script.js
@@ -1,42 +1,102 @@
 const tBody = document.querySelector("#tBody");
+const btn = document.querySelector("#submit")
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const finished = document.querySelector("#finished");
 
-function addBook (id, title, author, pages, finished) {
-    let row = document.createElement("tr");
+const exampleBook1 = {
+    id: crypto.randomUUID(),
+    title: "The You You Never Were!",
+    author: "Katy Perry",
+    pages: 458,
+    finished: true
+};
+const exampleBook2 = {
+    id: crypto.randomUUID(),
+    title: "Balls in the Field",
+    author: "Henry Harrenstein",
+    pages: 349,
+    finished: false
+};
 
-    let hId = document.createElement("td");
-    hId.textContent = `${id}`
-    row.appendChild(hId);
+let myLibrary = [exampleBook1, exampleBook2];
 
-    let hTitle = document.createElement("td");
-    hTitle.textContent = `${title}`
-    row.appendChild(hTitle);
-
-    let hAuthor = document.createElement("td");
-    hAuthor.textContent = `${author}`
-    row.appendChild(hAuthor);
-
-    let hPages = document.createElement("td");
-    hPages.textContent = `${pages}`
-    row.appendChild(hPages);
-
-    let hFinished = document.createElement("td");
-    if (finished) {
-        hFinished.textContent = "Yes"
-    } else {
-        hFinished.textContent = "No"
+function Book(title, author, pages, finished) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
     }
-    hFinished.addEventListener("click", () => {
-        if (hFinished.textContent == "No") {
-        hFinished.textContent = "Yes";
-    } else {
-        hFinished.textContent = "No";
-    }
-    })
-    hFinished.classList.add("finished")
-    row.appendChild(hFinished);
-
-    tBody.appendChild(row);
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.finished = finished;
 }
 
-addBook(29784, "My American Summer", "Katy Perry", 768, true);
-addBook(29781, "Help! I Need Somebody, It's Getting Crazy and My Head is Hot, Whhhhhaaaaaaaaaa", "Paul", 389, false);
+function addBookToLibrary(title, author, pages, finished) {
+    myLibrary.push(new Book (title, author, pages, finished));
+}
+
+function makeTable () {
+    tBody.textContent = "";
+
+    myLibrary.forEach(function (book) {
+        let row = document.createElement("tr");
+
+        let hId = document.createElement("td");
+        hId.textContent = `${book.id.substr(0, 8)}`
+        row.appendChild(hId);
+
+        let hTitle = document.createElement("td");
+        hTitle.textContent = `${book.title}`
+        row.appendChild(hTitle);
+
+        let hAuthor = document.createElement("td");
+        hAuthor.textContent = `${book.author}`
+        row.appendChild(hAuthor);
+
+        let hPages = document.createElement("td");
+        hPages.textContent = `${book.pages}`
+        row.appendChild(hPages);
+
+        let hFinished = document.createElement("td");
+        if (book.finished) {
+            hFinished.textContent = "Yes";
+        } else {
+            hFinished.textContent = "No";
+        }
+        hFinished.addEventListener("click", () => {
+            if (hFinished.textContent == "No") {
+            hFinished.textContent = "Yes";
+        } else {
+            hFinished.textContent = "No";
+        }
+        });
+        hFinished.classList.add("finished")
+        row.appendChild(hFinished);
+
+        let hRemove = document.createElement("td");
+        hRemove.id = myLibrary.indexOf(book);
+        hRemove.addEventListener("click", () => {
+            myLibrary.splice(hRemove.id, 1);
+            makeTable();
+        });
+        hRemove.textContent = `×`
+        hRemove.classList.add("removeSymbol")
+        row.appendChild(hRemove);
+
+        tBody.appendChild(row);
+    });
+}
+
+btn.addEventListener("click", () => {
+    addBookToLibrary(title.value, author.value, pages.value, finished.checked);
+    makeTable();
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    finished.checked = false;
+    event.preventDefault();
+})
+
+makeTable();
